850aee37761edfc0bb1d3ac61e4c082adc63d8ab
Achmat Samsodien <achmat@cell-life.org>
2014-03-12 19:32:17 +0200

    added Vertical property and Changed Scan Barcode to Scan

--- src/org/celllife/stockout/app/activities/PinActivity.java
+++ src/org/celllife/stockout/app/activities/PinActivity.java
@@ -1,9 +1,23 @@
 package org.celllife.stockout.app.activities;
 
+
+import org.celllife.stockout.app.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by achmat on 2014/03/12.
  */
-public class PinActivity {
+public class PinActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pin);
+
+
+
+    }
 
 }
