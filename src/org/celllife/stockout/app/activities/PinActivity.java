2f3dea22a41102067c379c972426fd88a89c516c
Achmat Samsodien <achmat@cell-life.org>
2014-03-17 08:40:14 +0200

    PinActivity changes

--- src/org/celllife/stockout/app/activities/PinActivity.java
+++ src/org/celllife/stockout/app/activities/PinActivity.java
@@ -3,11 +3,11 @@ package org.celllife.stockout.app.activities;
 
 import org.celllife.stockout.app.R;
 import org.celllife.stockout.app.activities.ScanActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Button;
-import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
@@ -19,31 +19,39 @@ public class PinActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.pin);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pin);
 
         final EditText passwd = (EditText) findViewById(R.id.pin);
-        final Button confirmButton = (Button) findViewById(R.id.confirm_button);
-        final Button cancelButton = (Button) findViewById(R.id.cancel_button);
 
-	private void setupScanButton(final View orderView) {
-        Button startScanBtn = (Button) orderView.findViewById (R.id.scan_button);
-        startScanBtn.setOnClickListener(new OnClickListener() {
-           @Override
-          public void onClick(View v) {
-            	Toast.makeText(orderView.getContext(), R.string.loading_scanner, Toast.LENGTH_LONG).show();
+    }
+
+    private void setupScanButton(final View orderView) {
+        Button confirmButton = (Button) orderView.findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 startScanActivity();
-          }
-      });
-	}
+            }
+        });
+    }
+
+    private void setupCancelButton(final View orderView) {
+        Button cancelButton = (Button) orderView.findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startScanActivity();
+            }
+        });
+    }
 
     private void startScanActivity() {
-       Intent intent = new Intent(orderView.getContext(), ScanActivity.class);
+        Intent intent = new Intent(this, ScanActivity.class);
         startActivity(intent);
     }
 
 
-
-    }
-
 }
+
+
