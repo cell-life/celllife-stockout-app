e892e5a4b3d639c60f62ee9339d398e8fa8e13a2
Dagmar Timler <dagmar.timler@gmail.com>
2014-03-17 10:37:45 +0200

    implemented return from scan activity

--- src/org/celllife/stockout/app/activities/ScanActivity.java
+++ src/org/celllife/stockout/app/activities/ScanActivity.java
@@ -1,6 +1,9 @@
 package org.celllife.stockout.app.activities;
 
 import org.celllife.stockout.app.R;
+import org.celllife.stockout.app.domain.Drug;
+import org.celllife.stockout.app.manager.StockTakeManager;
+import org.celllife.stockout.app.manager.StockTakeManagerImpl;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,6 +17,7 @@ import com.google.zxing.integration.android.IntentResult;
 public class ScanActivity extends Activity {
 
 	private TextView contentTxt;
+	private StockTakeManager stockManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class ScanActivity extends Activity {
 
 		// instantiate UI items
 		contentTxt = (TextView) findViewById(R.id.scan_content);
+		stockManager = new StockTakeManagerImpl(contentTxt.getContext());
 
 		// instantiate ZXing integration class
 		IntentIntegrator scanIntegrator = new IntentIntegrator(ScanActivity.this);
@@ -29,6 +34,7 @@ public class ScanActivity extends Activity {
 		scanIntegrator.initiateScan();
 	}
 
+	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		// retrieve result of scanning - instantiate ZXing object
 		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
@@ -39,11 +45,18 @@ public class ScanActivity extends Activity {
 			// get format name of data scanned
 			// String scanFormat = scanningResult.getFormatName();
 			// output to UI
-			contentTxt.setText("CONTENT: " + scanContent);
+			Drug drug = lookupDrug(scanContent);
+			contentTxt.setText("Content: " + scanContent+" Drug: "+drug);
+			
 		} else {
 			// invalid scan data or scan canceled
 			Toast toast = Toast.makeText(getApplicationContext(), R.string.no_barcode, Toast.LENGTH_SHORT);
 			toast.show();
 		}
+	}
+	
+	private Drug lookupDrug(String barcode) {
+		return stockManager.findDrugByBarcode(barcode);
+		//return new Drug("60011053", "Panado");
 	}
 }
