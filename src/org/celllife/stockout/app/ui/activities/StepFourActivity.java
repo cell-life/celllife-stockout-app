3c2139a7f931c2cda4f4d75d2c775550f0370b91
Achmat Samsodien <achmat@cell-life.org>
2014-05-08 11:02:05 +0200

    New Activities added

---
+++ src/org/celllife/stockout/app/ui/activities/StepFourActivity.java
@@ -0,0 +1,17 @@
+package org.celllife.stockout.app.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.celllife.stockout.app.R;
+
+
+public class StepFourActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.step_three_pt2);
+
+    }
+}
--- src/org/celllife/stockout/app/ui/activities/StepThreeActivity2.java
+++ src/org/celllife/stockout/app/ui/activities/StepThreeActivity2.java
@@ -1,7 +1,30 @@
 package org.celllife.stockout.app.ui.activities;
 
-/**
- * Created by achmat on 2014/04/25.
- */
-public class StepThreeActivity2 {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.celllife.stockout.app.R;
+
+public class StepThreeActivity2 extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.step_three_pt2);
+
+        Button proceedButton = (Button) findViewById(R.id.confirm_button);
+
+        proceedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent stepFour = new Intent (StepThreeActivity2.this, StepFourActivity.class);
+                startActivity(stepFour);
+            }
+        });
+
+    }
 }
+
+
