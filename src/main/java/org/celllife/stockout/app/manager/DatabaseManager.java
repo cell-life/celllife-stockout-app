d0d7ee770af65a31bb194dde9a22dfc2986fa8c0
Dagmar Timler <dagmar@cell-life.org>
2014-06-25 14:29:31 +0200

    INTERNAL-690: force the initialise of the DatabaseManager, regardless of whether db is null or not

--- src/main/java/org/celllife/stockout/app/manager/DatabaseManager.java
+++ src/main/java/org/celllife/stockout/app/manager/DatabaseManager.java
@@ -13,6 +13,7 @@ import org.celllife.stockout.app.database.framework.DatabaseOpenHelper;
 import org.celllife.stockout.app.database.framework.TableHelper;
 
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Use this to get access to the various data access objects.
@@ -53,21 +54,20 @@ public class DatabaseManager {
 	}
 	
 	public static void initialise(Context context) {
-		if (db == null) {
-			List<TableHelper<?>> tables = new ArrayList<TableHelper<?>>();
-			phoneDb = new PhoneTableAdapter();
-			tables.add(phoneDb);
-			drugDb = new DrugTableAdapter();
-			tables.add(drugDb);
-			alertDb = new AlertTableAdapter(drugDb);
-			tables.add(alertDb);
-			stockTakeDb = new StockTakeTableAdapter(drugDb);
-			tables.add(stockTakeDb);
-			stockReceivedDb = new StockReceivedTableAdapter(drugDb);
-			tables.add(stockReceivedDb);
-			stockHistoryDb = new StockHistoryTableAdapter(drugDb);
-			tables.add(stockHistoryDb);
-			db = new DatabaseOpenHelper(context, tables);
-		}
+	    Log.d("DatabaseManager", "Initalise with application context. db="+db);
+		List<TableHelper<?>> tables = new ArrayList<TableHelper<?>>();
+		phoneDb = new PhoneTableAdapter();
+		tables.add(phoneDb);
+		drugDb = new DrugTableAdapter();
+		tables.add(drugDb);
+		alertDb = new AlertTableAdapter(drugDb);
+		tables.add(alertDb);
+		stockTakeDb = new StockTakeTableAdapter(drugDb);
+		tables.add(stockTakeDb);
+		stockReceivedDb = new StockReceivedTableAdapter(drugDb);
+		tables.add(stockReceivedDb);
+		stockHistoryDb = new StockHistoryTableAdapter(drugDb);
+		tables.add(stockHistoryDb);
+		db = new DatabaseOpenHelper(context, tables);
 	}
 }
