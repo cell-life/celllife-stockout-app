d6a56e66f8e18786e7b8a5ea443e85571d60dad1
Chantell Witbooi <chantell@cell-life.org>
2014-07-07 15:11:12 +0200

    INTERNAL-610 formatting

--- src/main/java/org/celllife/stockout/app/ui/alarm/BootReceiver.java
+++ src/main/java/org/celllife/stockout/app/ui/alarm/BootReceiver.java
@@ -12,9 +12,9 @@ public class BootReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")){
-	    	// Get the alerts from the server
 	    	Intent alertService = new Intent(context, UpdateAlertService.class);
 	        context.startService(alertService);
+	        
 	        Intent offlineService = new Intent(context, OfflineService.class);
 	        context.startService(offlineService);
 	    	}
