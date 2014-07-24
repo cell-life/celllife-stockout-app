7096a723478584036cf238fb06819044b079f8ae
Achmat Samsodien <achmat@cell-life.org>
2014-07-24 14:49:49 +0200

    INTERNAL-717

--- src/main/java/org/celllife/stockout/app/ui/fragments/OrderFragment.java
+++ src/main/java/org/celllife/stockout/app/ui/fragments/OrderFragment.java
@@ -8,15 +8,20 @@ import org.celllife.stockout.app.domain.StockTake;
 import org.celllife.stockout.app.manager.AlertManager;
 import org.celllife.stockout.app.manager.ManagerFactory;
 import org.celllife.stockout.app.manager.StockTakeManager;
+import org.celllife.stockout.app.ui.activities.DrugDetailActivity;
 import org.celllife.stockout.app.ui.adapters.AlertListViewAdapter;
 import org.celllife.stockout.app.ui.adapters.StockListViewAdapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This fragment manages the order tab view which contains a list of alerts and a list of stock takes done in
@@ -54,15 +59,32 @@ public class OrderFragment extends ScanFragment {
 	    final ListView listview = (ListView) orderView.findViewById(R.id.drug_alert_list);
 	    AlertManager alertManager = ManagerFactory.getAlertManager();
 	    List<Alert> values = alertManager.getAlerts();
-	    Alert[] alerts = values.toArray(new Alert[values.size()]);
+	    final Alert[] alerts = values.toArray(new Alert[values.size()]);
 	    final AlertListViewAdapter adapter = new AlertListViewAdapter(orderView.getContext(), R.id.drug_alert_list, alerts);
 	    listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //selected item
+            Alert a = alerts[position];
+
+            //send data to new activity
+            Intent drugDetail = new Intent(OrderFragment.this.getActivity().getApplicationContext(), DrugDetailActivity.class);
+            drugDetail.putExtra("Alert",a);
+            startActivity(drugDetail);
+            }
+
+
+        });
 	    
 	    // setup alert count
 		final TextView alertCount = (TextView) orderView.findViewById(R.id.alert_count);
 		final TextView newAlerts = (TextView) orderView.findViewById(R.id.new_alerts);
 		alertCount.setText("("+alerts.length+") ");
 		newAlerts.setText(R.string.new_alerts);
+
+
+
 	}
 
 	private void setupStock(View stockView) {
